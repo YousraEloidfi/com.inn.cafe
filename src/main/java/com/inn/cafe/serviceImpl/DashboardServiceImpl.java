b2526dbe5344d7ce37d550111f90ac27700b18e2
yousra wadfy <ywadfy@gmail.com>
2023-06-30 19:22:35 +0100

    Projet cafe management

--- src/main/java/com/inn/cafe/serviceImpl/DashboardServiceImpl.java
+++ src/main/java/com/inn/cafe/serviceImpl/DashboardServiceImpl.java
@@ -21,7 +21,7 @@ public class DashboardServiceImpl implements DashboardService {
     ProductDao productDao;
 
     @Override
-    public ResponseEntity<Map<String, Object>> getCount() {
+    public ResponseEntity<Map<String, Object>> getCount(){
         Map<String , Object> map = new HashMap<>();
         map.put("category" , categoryDao.count());
         map.put("product" , productDao.count());
